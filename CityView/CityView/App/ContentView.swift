46b20f47c01870d250b8fc873ebe0bfe01048d14
Liang Wang <liangtowang@gmail.com>
2023-02-28 00:40:19 +0100

    Get current location and show it

--- CityView/CityView/App/ContentView.swift
+++ CityView/CityView/App/ContentView.swift
@@ -6,11 +6,14 @@
 //
 
 import SwiftUI
+import CoreLocation
+import CoreLocationUI
 
 struct ContentView: View {
   
   // MARK: - Property
   @State private var cities = [City]()
+  @StateObject var locationManager = LocationManager()
   
   // MARK: - Body
   var body: some View {
@@ -30,6 +33,29 @@ struct ContentView: View {
       .navigationBarTitle(Constants.General.appName, displayMode: .large)
     } //: NavigationView
     .navigationViewStyle(StackNavigationViewStyle())
+    
+    // Get current location
+    VStack {
+      if let myLocation = locationManager.location {
+        VStack {
+          Text("My location: \(myLocation.latitude), \(myLocation.longitude)")
+            .font(.callout)
+            .foregroundColor(.white)
+            .padding()
+            .background(.gray)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+        }
+      }
+      
+      LocationButton {
+        locationManager.requestLocation()
+      }
+      .frame(width: 180, height: 40)
+      .cornerRadius(30)
+      .symbolVariant(.fill)
+      .foregroundColor(.white)
+    }
+    .padding()
   }
 }
 

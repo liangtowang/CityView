e10aef66acd86ad375a030e65b0d9b5943b0fc85
Liang Wang <liangtowang@gmail.com>
2023-02-27 22:57:05 +0100

    Create project CityView

---
+++ CityView/CityView/CityViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CityViewApp.swift
+//  CityView
+//
+//  Created by Liang Wang on 2023-02-27.
+//
+
+import SwiftUI
+
+@main
+struct CityViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

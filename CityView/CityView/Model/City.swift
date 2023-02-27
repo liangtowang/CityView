39d22bee0ec172267c11279e78d671d23257ec79
Liang Wang <liangtowang@gmail.com>
2023-02-27 23:12:12 +0100

    Add a model for City

---
+++ CityView/CityView/Model/City.swift
@@ -0,0 +1,14 @@
+//
+//  City.swift
+//  CityView
+//
+//  Created by Liang Wang on 2023-02-27.
+//
+
+import Foundation
+
+struct City {
+  let name: String
+  let lat: Double
+  let lon: Double
+}

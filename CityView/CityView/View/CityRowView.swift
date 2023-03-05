5bb4684508750a30f0f037937925224ce9c1dc7e
Liang Wang <liangtowang@gmail.com>
2023-03-05 22:40:09 +0100

    Calculate distance to city from current location

--- CityView/CityView/View/CityRowView.swift
+++ CityView/CityView/View/CityRowView.swift
@@ -6,22 +6,54 @@
 //
 
 import SwiftUI
+import CoreLocation
+import CoreLocationUI
 
 struct CityRowView: View {
   
   // MARK: - Property
   @State var city: City
+  @State var distance = 0.0
+  
+  @StateObject var locationManager = LocationManager()
   
   // MARK: - Body
   var body: some View {
     VStack(alignment: .leading, spacing: Constants.General.spacing) {
       Text(city.name)
         .font(.title2)
-      Text("Latitude: \(city.lat)")
-        .font(.caption2)
-      Text("Longitude: \(city.lon)")
+      Text("Distance: \(distance, specifier: "%.1f") km")
         .font(.caption2)
     }
+    .onAppear {
+      distance = calculateDistance()
+    }
+  }
+  
+  // Calculate distance
+  func calculateDistance() -> Double {
+    var myLocation = CLLocation()
+    
+    // Get user location
+    if let location = locationManager.location {
+      
+      myLocation = CLLocation(latitude: location.latitude, longitude: location.longitude)
+      print("Current location latitude: \(myLocation.coordinate.latitude)")
+    } else {
+      myLocation = CLLocation(latitude: 59.437601, longitude: 18.066346)
+      print("Default lcoation latitude: \(myLocation.coordinate.latitude)")
+    }
+    //City's location
+    let cityLocation = CLLocation(latitude: city.lat, longitude: city.lon)
+    
+    //Measuring my distance to the city (in km)
+    let distance = cityLocation.distance(from: myLocation) / 1000
+    
+    //Display the result in km
+    print(String(format: "The distance to \(city.name) is %.01fkm \n", distance))
+    
+    return distance
+    
   }
 }
 
